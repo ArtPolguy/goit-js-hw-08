3a66d4ed48ac49905eb08320e0b723bf80d4d213
Artur Polguy <art.polguy@gmail.com>
2022-11-20 23:33:57 +0200

    update-gallery&video_js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import Simplelightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector(`.gallery`),
+  galleryMarkup: createGalleryMarkup(galleryItems),
+};
+
+refs.galleryContainer.insertAdjacentHTML(`afterbegin`, refs.galleryMarkup);
+
+refs.galleryContainer.addEventListener(`click`, onImgGalleryClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li style="list-style: none">
+        <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+      </li>`;
+    })
+    .join(' ');
+}
+function onImgGalleryClick(evt) {
+  evt.preventDefault();
+
+  const isElClick = evt.target.classList.contains(`gallery__image`);
+  if (!isElClick) {
+    return;
+  }
+  //   const parent = evt.target.closest(`a`);
+}
+
+const lightbox = new Simplelightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+
+player.on('timeupdate', setCurrentTime);
+
+function setCurrentTime(evt) {
+  //   console.log(evt.seconds);
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+  //   console.log(localStorage);
+}
+
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+console.log(getCurrentTime);
+
+player.setCurrentTime(getCurrentTime);
+
+console.log(player.setCurrentTime);
